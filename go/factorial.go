b4f6eb889190f1231ccab1e556932d521fe01f3f
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2022-12-28 15:47:23 +0100

    Add factorial

--- go/factorial.go
+++ go/factorial.go
@@ -18,7 +18,7 @@ func factorial(n int) int {
 	if n == 0 {
 		return 1
 	}
-	return n * factorial(n-1)
+	return n * factorial(n - 1)
 }
 
 // main is the entry point for the program.
---
+++ rust/factorial.rs
@@ -0,0 +1,24 @@
+// Recursive Rust program to calculate the factorial of a given number.
+//
+// @File = factorial.rs
+// @Date = 2022-12-28
+// @Author = Jannis Metrikat
+// @Version = 1.0
+
+use std::env;
+
+// Calculates the factorial of a given number.
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+// Main function.
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let n: u64 = args[1].parse().unwrap();
+    println!("{}", factorial(n));
+}
